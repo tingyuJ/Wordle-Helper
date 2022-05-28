492e0695061d803fb478a6d6333434c17d5d3e0a
TingyuSystex <103096972+TingyuSystex@users.noreply.github.com>
2022-05-29 04:03:03 +0800

    Commit

--- FP/wordle/static/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('input').keyup(function(){
-        this.value = this.value.toUpperCase();
-        if(this.value.length==1){
-            $(this).next().focus();
-        }
-    });
-});
